016ace5db97ae938c82d75d9ca3f4f3eabbca259
BaximusB <sehnus1997@gmail.com>
2022-01-17 15:35:05 +0100

    Q-Learning Training Loop completed.

--- src/agent.py
+++ src/agent.py
@@ -1,6 +1,6 @@
 import numpy as np
 from actions import *
-
+import time
 
 class Agent:
     def __init__(self, rob):
@@ -8,11 +8,14 @@ class Agent:
         Each state has multiple Q-values, therefore q_values is dict() object with keys=state and values=list of q-vals
         num_states likely equals 6: 5 observing states and one state when nothing is observed
         """
-        self.gamma = None
-        self.eps = 0.5  # high epsilon for testing purposes. subject to change
-        self.alpha = None
+        self.gamma = 0.9
+        self.eps = 0.2
+        self.alpha = 0.5
         self.rob = rob
-        self.q_values = {x: [0, 0, 0] for x in range(6)}    # for now assuming three actions
+        self.q_values = {x: [1, 0, 0, 0, 0] for x in range(4)}    # for now assuming five actions
+        self.current_state = None   # state agent is in
+        self.observed_state = None  # state agent is in after taking action a
+        self.last_action = None
 
 
     def get_state(self):
@@ -21,18 +24,35 @@ class Agent:
         States: observed front, right, left, front&right, front&left, nothing
         returns int
         """
-        pass
+        sensors = [-np.inf if x == False else x for x in self.rob.read_irs()[3:]]   #only take front sensors
+        if all([x == -np.inf for x in sensors]):
+            return 0    # state 0 is no observations
+
+        elif np.argmax(sensors) == 0 or np.argmax(sensors) == 1:
+            if sensors[0] < 0.15 or sensors[1] < 0.15:
+                return 1       # object to the right
+
+        elif np.argmax(sensors) == 2:
+            if sensors[2] < 0.15:
+                return 2      # object directly in front
+
+        elif np.argmax(sensors) == 3 or np.argmax(sensors) == 4:
+            if sensors[3] < 0.15 or sensors[4] < 0.15:
+                return 3      # object to the left
+
+        return 0
 
 
     def action(self, state):
         """
         Use epsilon greedy policy to determine action
-        runs certain action based on index, currently only three actions
+        runs certain action based on index, currently 5 actions
         """
         if np.random.binomial(1, self.eps) == 1:
-            action_index = np.random.choice([0, 1, 2])  #pick a random action based on the index. see actions.py
+            action_index = np.random.choice([0, 1, 2, 3, 4])  #pick a random action based on the index. see actions.py
         else:
             action_index = np.argmax(self.q_values[state])  # pick the best action based on q-values
+        self.last_action = action_index
         select_action(self.rob, action_index)
 
 
@@ -42,23 +62,52 @@ class Agent:
         Positive if not observed (could be 1, 5, 10)
         returns int
         """
-        pass
+        if self.observed_state == 0:
+            if self.last_action == 0:
+                return 3
+            else:
+                return 1
+        else:
+            return -1
 
 
-    def calc_Q_values(self):
+
+    def calc_Q_values(self, action, reward):
         """
-        Use formula to update self.q_values
-        returns nothing
+        update q_values
         """
-        pass
+        state = self.current_state
+        _state = self.observed_state
+        current_q = self.q_values[state][action]
+        next_q = np.argmax(self.q_values[_state])
+        a = self.alpha
+        g = self.gamma
+        self.q_values[state][action] = current_q + a*(reward + g*next_q - current_q)
 
 
-    def train_loop(self, episodes=15, steps=100):
-        """
-        Combines all of the above to run a training loop and update the Q-values
-        Does 15 training epochs with 100 steps per epoch
-        returns nothing, should likely store values of self.q_values in file
-        """
-        pass
+
+def train_loop(rob, episodes=15, steps=50):
+    """
+    Combines all of the above to run a training loop and update the Q-values
+    Does 15 training epochs with 50 steps per epoch
+    returns nothing, should likely store values of self.q_values in file
+    """
+    agent = Agent(rob)
+    for episode in range(episodes):
+        agent.rob.play_simulation()
+        time.sleep(1)
+        agent.current_state = agent.get_state()
+        for step in range(steps):
+            agent.action(agent.current_state)
+            time.sleep(1)
+            agent.observed_state = agent.get_state()
+            reward = agent.get_reward()
+            agent.calc_Q_values(agent.last_action, reward)
+            agent.current_state = agent.observed_state
+
+        for key, value in agent.q_values.items():
+            print(f"State {key}, Qvalues: {value}")
+        agent.rob.stop_world()
+        time.sleep(1)
 
 
--- src/send_commands.py
+++ src/send_commands.py
@@ -11,6 +11,7 @@ import sys
 import signal
 import prey
 
+from agent import *
 
 def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
@@ -20,52 +21,29 @@ def main():
     signal.signal(signal.SIGINT, terminate_program)
 
     # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
-    rob = robobo.SimulationRobobo().connect(address='localhost', port=19997)
+    rob = robobo.SimulationRobobo().connect(address='', port=19997)
 
-    rob.play_simulation()
+    # rob.play_simulation()
 
-    # Following code moves the robot
-    for i in range(10):
-            print("robobo is at {}".format(rob.position()))
-            if i%2 == 0:
-                rob.move(25,10, 5000)
-            if i%3 == 0:
-                rob.move(10,25,5000)
-            rob.move(25, 25, 5000)
-   
-    print("robobo is at {}".format(rob.position()))
-    rob.sleep(1)
-
-    # Following code moves the phone stand
-    rob.set_phone_pan(343, 100)
-    rob.set_phone_tilt(109, 100)
-    # time.sleep(1)
-    rob.set_phone_pan(11, 100)
-    rob.set_phone_tilt(26, 100)
-
-    # Following code makes the robot talk and be emotional
-    rob.set_emotion('happy')
-    rob.talk('Hi, my name is Robobo')
-    rob.sleep(1)
-    rob.set_emotion('sad')
+    train_loop(rob)
 
     # Following code gets an image from the camera
-    image = rob.get_image_front()
-    # IMPORTANT! `image` returned by the simulator is BGR, not RGB
-    cv2.imwrite("test_pictures.png",image)
+    # image = rob.get_image_front()
+    # # IMPORTANT! `image` returned by the simulator is BGR, not RGB
+    # cv2.imwrite("test_pictures.png",image)
 
-    time.sleep(0.1)
+    # time.sleep(0.1)
 
     # IR reading
-    for i in range(100):
-        print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
-        time.sleep(0.1)
+    # for i in range(100):
+    #     print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
+    #     time.sleep(0.1)
 
     # pause the simulation and read the collected food
-    rob.pause_simulation()
-    
-    # Stopping the simualtion resets the environment
-    rob.stop_world()
+    # rob.pause_simulation()
+    #
+    # # Stopping the simualtion resets the environment
+    # rob.stop_world()
 
 
 if __name__ == "__main__":
