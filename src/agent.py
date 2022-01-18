2a9758dcb610604ba7662a656aa62eadd968c1ee
NielsProc <81763883+NielsProc@users.noreply.github.com>
2022-01-18 09:17:21 +0100

    Update agent.py
    Changed the code to read sensors once instead of 3 times per state. And errors came back when initializing the world and reading the sensors right away so increased sleep time after initialization

--- src/agent.py
+++ src/agent.py
@@ -30,7 +30,10 @@ class Agent:
         States: observed front, right, left, front&right, front&left, nothing
         returns int
         """
-        sensors = [np.inf if x == False else x for x in self.rob.read_irs()[3:]]   #only take front sensors
+        read = self.rob.read_irs()[3:]
+        sensors = [np.inf if x == False else x for x in read]   #only take front sensors
+        self.sensors_triggered = sum([x < self.threshold for x in read]) - self.previous_sensors
+        self.previous_sensors = sum([x < self.threshold for x in read])
         if all([x == np.inf for x in sensors]):
             return 0    # state 0 is no observations
 
@@ -75,8 +78,6 @@ class Agent:
                 return 1
 
         else:
-            self.sensors_triggered = sum([x < self.threshold for x in self.rob.read_irs()[3:]]) - self.previous_sensors
-            self.previous_sensors = sum([x < self.threshold for x in self.rob.read_irs()[3:]])
             return -1 * self.sensors_triggered
 
 
@@ -104,7 +105,7 @@ def train_loop(rob, episodes=5, steps=50):
     agent = Agent(rob)
     for episode in range(episodes):
         agent.rob.play_simulation()
-        time.sleep(1)
+        time.sleep(3)
         agent.current_state = agent.get_state()
         for step in range(steps):
             agent.action(agent.current_state)
