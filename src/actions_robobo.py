132c8010cf898c2b1055d9ba0be668ae77db7e3f
BaximusB <sehnus1997@gmail.com>
2022-01-19 20:12:49 +0100

    Actions for the hardware

---
+++ src/actions_robobo.py
@@ -0,0 +1,27 @@
+def forward(rob):
+    rob.move(10, 10, 1500)
+
+def turn_left(rob):
+    rob.move(-10, 10, 1000)
+
+# def turn_left_long(rob):
+#     rob.move(-10, 10, 1000)
+
+def turn_right(rob):
+    rob.move(10, -10, 1500)
+#
+# def turn_right_long(rob):
+#     rob.move(10, -10, 1000)
+
+def select_action(rob, index):
+    if index == 0:
+        forward(rob)
+    elif index == 1:
+        turn_left(rob)
+    # elif index == 2:
+    #     turn_left_long(rob)
+    elif index == 2:
+        turn_right(rob)
+    # elif index == 4:
+    #     turn_right_long(rob)
+
