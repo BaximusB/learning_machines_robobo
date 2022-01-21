06f73a955cb5ccceaf4512d51e66c85ba8bfe734
BaximusB <sehnus1997@gmail.com>
2022-01-21 08:55:11 +0100

    Right turn a bit stronger than left

--- src/actions.py
+++ src/actions.py
@@ -2,13 +2,13 @@ def forward(rob):
     rob.move(10, 10, 2000)
 
 def turn_left(rob):
-    rob.move(-10, 10, 500)
+    rob.move(-10, 10, 600)
 
 def turn_left_long(rob):
     rob.move(-10, 10, 1000)
 
 def turn_right(rob):
-    rob.move(10, -10, 500)
+    rob.move(10, -10, 900)
 
 def turn_right_long(rob):
     rob.move(10, -10, 1000)
@@ -19,9 +19,9 @@ def select_action(rob, index):
     elif index == 1:
         turn_left(rob)
     elif index == 2:
-        turn_left_long(rob)
-    elif index == 3:
         turn_right(rob)
-    elif index == 4:
-        turn_right_long(rob)
+    # elif index == 3:
+    #     turn_right(rob)
+    # elif index == 4:
+    #     turn_right_long(rob)
 
